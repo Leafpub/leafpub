c4a7ecf33557b05bfd2bf60badfed9a4967ba0e9
Pablo Joán Iglesias <iglesias.pablo10@gmail.com>
2016-07-31 12:05:07 -0400

    All these checks can be done on composer

--- app/source/runtime.php
+++ app/source/runtime.php
@@ -9,23 +9,3 @@ error_reporting(POSTLEAF_DEV ? E_ALL & ~E_NOTICE : 0);
 $loader = require_once __DIR__ . '/vendor/autoload.php';
 $loader->setPsr4('Postleaf\\', __DIR__ . '/classes');
 $loader->register();
-
-// Check for GD extension
-if(!extension_loaded('gd')) {
-    exit('Postleaf requires the GD extension.');
-}
-
-// Check for mbstring extension
-if(!extension_loaded('mbstring')) {
-    exit('Postleaf requires the Multibyte String extension.');
-}
-
-// Check for OpenSSL extension
-if(!extension_loaded('openssl')) {
-    exit('Postleaf requires the OpenSSL extension.');
-}
-
-// Check for PDO MySQL extension
-if(!extension_loaded('pdo_mysql')) {
-    exit('Postleaf requires the PDO extension with the MySQL driver.');
-}
