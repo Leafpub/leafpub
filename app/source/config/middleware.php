8935e401a31d382835d11dd3229b14a5c32f8785
karsasmus <karsasmus82@gmail.com>
2016-12-18 19:58:33 +0100

    Removed unnecessary code

--- app/source/config/middleware.php
+++ app/source/config/middleware.php
@@ -3,12 +3,5 @@ namespace Leafpub;
 
 $app->add('Leafpub\Middleware:removeTrailingSlashes');
 $app->add('Leafpub\Middleware::maintenance');
-$app->add(function($req, $res, $next){
-    Leafpub::on(Events\Post\BeforeRender::NAME, function($event){
-        $data = &$event->getEventData();
-        //var_dump($data); exit;
-        //$data['special_vars'] = array();
-    });
-    return $next($req, $res);
-});
+
 ?>
