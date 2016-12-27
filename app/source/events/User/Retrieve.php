de23a7bf7e6377ae0dec0f6d768ad8c8b35d3786
karsasmus <karsasmus82@gmail.com>
2016-12-27 18:14:03 +0100

    New events

---
+++ app/source/events/User/Retrieve.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Leafpub: Simple, beautiful publishing. (https://leafpub.org)
+ *
+ * @link      https://github.com/Leafpub/leafpub
+ * @copyright Copyright (c) 2016 Leafpub Team
+ * @license   https://github.com/Leafpub/leafpub/blob/master/LICENSE.md (GPL License)
+ */
+namespace Leafpub\Events\User;
+
+use Leafpub\Events\LeafpubEvent;
+
+class Retrieve extends LeafpubEvent {
+    const NAME = 'user.retrieve';
+}
+
+?>
