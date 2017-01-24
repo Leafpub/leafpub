b25138feeefa0095ee08c0ab9c8c73bcf04358b7
karsasmus <karsasmus82@gmail.com>
2017-01-24 07:37:20 +0100

    Adding event classes for uploads

---
+++ app/source/events/Upload/ManyRetrieve.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Leafpub: Simple, beautiful publishing. (https://leafpub.org)
+ *
+ * @link      https://github.com/Leafpub/leafpub
+ * @copyright Copyright (c) 2017 Leafpub Team
+ * @license   https://github.com/Leafpub/leafpub/blob/master/LICENSE.md (GPL License)
+ */
+namespace Leafpub\Events\Upload;
+
+use Leafpub\Events\LeafpubEvent;
+
+class ManyRetrieve extends LeafpubEvent {
+    const NAME = 'uploads.retrieve';
+}
+
+?>
