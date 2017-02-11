04e9e330c83e4a53f4589dc526e1c322272a4dbe
karsasmus <karsasmus82@gmail.com>
2017-02-11 13:02:06 +0100

    Save charset and driver

--- app/source/defaults/default.database.php
+++ app/source/defaults/default.database.php
@@ -1,9 +1,11 @@
 <?php
 return [
+    'driver' => '{{driver}}',
     'host' => '{{host}}',
     'port' => '{{port}}',
     'database' => '{{database}}',
     'user' => '{{user}}',
     'password' => '{{password}}',
-    'prefix' => '{{prefix}}'
+    'prefix' => '{{prefix}}',
+    'charset' => 'utf8mb4'
 ];
