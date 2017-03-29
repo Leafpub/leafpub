39635591129813540e31308af8422d0ee3d0d53b
karsasmus <karsasmus82@gmail.com>
2017-03-29 09:31:14 +0200

    don't include jquery into lib.js, so we can include it into <head>

--- app/source/scripts/lib.js
+++ app/source/scripts/lib.js
@@ -1,5 +1,5 @@
 // Libs
-// @include node_modules/jquery/dist/jquery.min.js
+// include node_modules/jquery/dist/jquery.min.js
 // @include node_modules/jquery-ui/dist/jquery-ui.min.js
 // @include node_modules/tether/dist/js/tether.min.js
 // @include node_modules/bootstrap/dist/js/bootstrap.min.js
