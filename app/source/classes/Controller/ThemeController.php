657191c0909fbdd150da09c66e46f29268e215ad
karsasmus <karsasmus82@gmail.com>
2017-03-31 08:16:33 +0200

    Added preview var

--- app/source/classes/Controller/ThemeController.php
+++ app/source/classes/Controller/ThemeController.php
@@ -125,17 +125,20 @@ class ThemeController extends Controller {
     *
     **/
     public function post($request, $response, $args) {
+        $preview = Session::isAuthenticated() && isset($request->getParams()['preview']);
         $html = Post::render($args['post'], [
             // Render this post as a preview if the user is logged in and ?preview is in the URL
-            'preview' => Session::isAuthenticated() && isset($request->getParams()['preview'])
+            'preview' => $preview
         ]);
 
         if ($html === false){
             return $this->notFound($request, $response);
         } else {
-            $ev = new PostViewed(['post' => $args['post'], 'request' => $request]);
-            \Leafpub\Leafpub::dispatchEvent(PostViewed::NAME, $ev);
-            $response->write($html);
+            if (!$preview){
+                $ev = new PostViewed(['post' => $args['post'], 'request' => $request]);
+                \Leafpub\Leafpub::dispatchEvent(PostViewed::NAME, $ev);
+            }
+            return $response->write($html);
         }
     }
 
