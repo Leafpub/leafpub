20aca3219c1bff02f07842fa1ead27b11d2a856e
karsasmus <karsasmus82@gmail.com>
2017-04-09 07:50:59 +0200

    we should catch our own thrown errors  😉

--- app/source/classes/Importer.php
+++ app/source/classes/Importer.php
@@ -14,17 +14,25 @@ class Importer extends Leafpub {
         Models\Setting::edit(['name' => 'maintenance', 'value' => 'off']);
         // 2. ini_set
         $old_execution_time = ini_get('max_execution_time');
-        ini_set('max_execution_time', '600');        
-        // 5. Parse file and import data
-        $importer = ImportFactory::factory($dropin, $file);
-        $importer->setOptions($options);
-        $importer->parseFile();
-        $ret = $importer->importData();
-        // 8. unlink $file
-        unlink($file);
-        // 9. Set maintenance mode off
-        ini_set('max_execution_time', $old_execution_time);
-        Models\Setting::edit(['name' => 'maintenance', 'value' => 'off']);
-        return $ret;
+        ini_set('max_execution_time', '600');  
+        try {      
+            // 5. Parse file and import data
+            $importer = ImportFactory::factory($dropin, $file);
+            $importer->setOptions($options);
+            $importer->parseFile();
+            $ret = $importer->importData();
+            // 8. unlink $file
+            unlink($file);
+            // 9. Set maintenance mode off
+            ini_set('max_execution_time', $old_execution_time);
+            Models\Setting::edit(['name' => 'maintenance', 'value' => 'off']);
+            return $ret;
+        } catch (\Exception $e){
+            self::getLogger()->error($e->getMessage());
+            return [
+                'succeed' => [], 
+                'failed' => [['error', $e->getMessage()]]
+            ];
+        }
     }
 }
