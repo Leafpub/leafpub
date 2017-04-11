bfe433fb83cbc50ad9fc4ae691d5211c571d7b98
Marc Apfelbaum <karsasmus82@gmail.com>
2017-04-11 20:00:03 +0000

    Added amp endpoint

--- app/source/classes/Controller/ThemeController.php
+++ app/source/classes/Controller/ThemeController.php
@@ -141,6 +141,25 @@ class ThemeController extends Controller {
             return $response->write($html);
         }
     }
+    
+    public function ampify($request, $response, $args) {
+        $preview = Session::isAuthenticated() && isset($request->getParams()['preview']);
+        $html = Post::render($args['post'], [
+            // Render this post as a preview if the user is logged in and ?preview is in the URL
+            'preview' => $preview,
+            'amp' => true
+        ]);
+
+        if ($html === false){
+            return $this->notFound($request, $response);
+        } else {
+            if (!$preview){
+                $ev = new PostViewed(['post' => $args['post'], 'request' => $request]);
+                \Leafpub\Leafpub::dispatchEvent(PostViewed::NAME, $ev);
+            }
+            return $response->write($html);
+        }
+    }
 
     /**
     * Renders the search results
